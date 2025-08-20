961eae5cd32f9b021688bf769614f370a44508d1
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-08-20 08:24:04 +0100

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,64 +1,34 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.aggressiveCows(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    public static int aggressiveCows(int[] stalls, int k) {
-        int n = stalls.length; //size of array
-        //sort the stalls[]:
+    public int aggressiveCows(int[] stalls, int k) {
         Arrays.sort(stalls);
-
-        int low = 1, high = stalls[n - 1] - stalls[0];
-        //apply binary search:
+        int len = stalls.length;
+        
+        int low = 0;
+        int high = stalls[len - 1] - stalls[0]; // max distance possible
+        
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (canWePlace(stalls, mid, k) == true) {
-                low = mid + 1;
-            } else high = mid - 1;
+            int m = (low + high) / 2;
+            
+            if (canWePlaceCows(stalls, m, k)) low = m + 1;
+            else high = m - 1;
         }
+        
         return high;
     }
     
-    public static boolean canWePlace(int[] stalls, int dist, int cows) {
-        int n = stalls.length; //size of array
-        int cntCows = 1; //no. of cows placed
-        //position of last placed cow.
-        int last = stalls[0]; 
-        for (int i = 1; i < n; i++) {
-            if (stalls[i] - last >= dist) {
-                cntCows++; //place next cow.
-                last = stalls[i]; //update the last location.
+    private boolean canWePlaceCows(int[] arr, int dis, int k) {
+        int placedCowsCount = 1;
+        int lastPlacedCow = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - lastPlacedCow >= dis) {
+                placedCowsCount++;
+                lastPlacedCow = arr[i];
             }
-            if (cntCows >= cows) return true;
         }
+        
+        if (placedCowsCount >= k) return true;
+        
         return false;
     }
 }
