a24df0ea86b0b6d96e0b13ac1f94db0114abd4c1
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-10-11 08:30:15 +0100

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -1,22 +1,24 @@
 class Solution {
     static int inversionCount(int arr[]) {
+        // Code Here
         return mergeSort(arr, 0, arr.length - 1);
     }
     
-    private static int mergeSort(int[] a, int l, int h) {
+    static int mergeSort(int[] arr, int l, int h) {
         int count = 0;
         
         if (l >= h) return count;
         
-        int mid = (l + h) / 2;
-        count += mergeSort(a, l, mid);
-        count += mergeSort(a, mid + 1, h);
-        count += merge(a, l, mid, h);
+        int m = (l + h) / 2;
+        
+        count += mergeSort(arr, l, m);
+        count += mergeSort(arr, m + 1, h);
+        count +=merge(arr, l, m, h);
         
         return count;
     }
     
-    private static int merge(int[] a, int l, int m, int h) {
+    static int merge(int[] arr, int l, int m, int h) {
         ArrayList<Integer> list = new ArrayList<>();
         int left = l;
         int right = m + 1;
@@ -24,28 +26,28 @@ class Solution {
         int count = 0;
         
         while (left <= m && right <= h) {
-            if (a[left] <= a[right]) {
-                list.add(a[left]);
+            if (arr[left] <= arr[right]) {
+                list.add(arr[left]);
                 left++;
             } else {
-                list.add(a[right]);
+                list.add(arr[right]);
                 count += m - left + 1;
                 right++;
             }
         }
         
         while (left <= m) {
-            list.add(a[left]);
+            list.add(arr[left]);
             left++;
         }
         
         while (right <= h) {
-            list.add(a[right]);
+            list.add(arr[right]);
             right++;
         }
         
         for (int i = l; i <= h; i++) {
-            a[i] = list.get(i - l);
+            arr[i] = list.get(i - l);
         }
         
         return count;
