4206a8217e0b4b9d28bc732675c3039158773812
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-11-17 19:19:16 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -1,55 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GfG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            String s = sc.next();
-            int k = sc.nextInt();
-            Solution obj = new Solution();
-            System.out.println(obj.longestkSubstr(s, k));
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    public int longestkSubstr(String s, int k) {
+    public int longestKSubstr(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
+        int ws = 0;
+        int we = 0;
+        int n = s.length();
+        int ans = -1;
         
-        int left = 0;
-        int right = 0;
-        int max = -1;
-        while (right < s.length()) {
-            char c = s.charAt(right);
+        for (we = 0; we < n; we++) {
+            char c = s.charAt(we);
             map.put(c, map.getOrDefault(c, 0) + 1);
             
-            while (map.size() > k) {
-                char leftC = s.charAt(left);
-                map.put(leftC, map.getOrDefault(leftC, 0) - 1);
+            if (map.size() > k) {
+                char leftChar = s.charAt(ws);
+                map.put(leftChar, map.get(leftChar) - 1);
                 
-                if (map.get(leftC) == 0) map.remove(leftC);
-                
-                left++;
+                if (map.get(leftChar) == 0) map.remove(leftChar);
+                ws++;
             }
             
             if (map.size() == k) {
-                max = Math.max(max, right - left + 1);
+                ans = Math.max(ans, we - ws + 1);
             }
-            
-            right++;
         }
         
-        return max;
+        return ans;
+        
     }
 }
