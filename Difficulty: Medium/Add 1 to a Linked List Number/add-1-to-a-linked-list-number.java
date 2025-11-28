7c73e79bb83e6449cedb1084e85a386a8e32b635
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-11-28 12:15:43 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -1,49 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-class GfG {
-    public static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data);
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int n = str.length;
-            Node head = new Node(Integer.parseInt(str[0]));
-            Node tail = head;
-            for (int i = 1; i < n; i++) {
-                tail.next = new Node(Integer.parseInt(str[i]));
-                tail = tail.next;
-            }
-            Solution obj = new Solution();
-            head = obj.addOne(head);
-            printList(head);
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 /*
 class Node{
     int data;
@@ -57,30 +11,29 @@ class Node{
 */
 
 class Solution {
-    // return the carry
-    private int f(Node temp) {
-        if (temp == null) return 1;
-        
-        int carry = f(temp.next);
-        
-        temp.data = temp.data + carry;
-        
-        if (temp.data < 10) return 0;
-        
-        temp.data = 0;
-        
-        return 1;
-    }
-    
     public Node addOne(Node head) {
-        int carry = f(head);
+        int carry = helper(head);
         
         if (carry == 1) {
-            Node newHead = new Node(1);
-            newHead.next = head;
-            return newHead;
+            Node node = new Node(1);
+            node.next = head;
+            return node;
         }
         
         return head;
+    }
+    
+    private int helper(Node node) {
+        if (node == null) return 1;
+        
+        int carry = helper(node.next);
+        
+        node.data = node.data + carry;
+        
+        if (node.data < 10) return 0;
+        
+        node.data = 0;
+        
+        return 1;
     }
 }
