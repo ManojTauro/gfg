27ebe6f3bf3ebb73f7233d880bc8613f1489b663
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-11-28 23:44:02 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.java
+++ Difficulty: Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.java
@@ -1,108 +1,44 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.*;
-import java.io.*;
-
-// a node of the doubly linked list
-class Node {
-    int data;
-    Node next, prev;
-}
-
-class GFG {
-
-    /* Function to insert a node at the beginning
-        of the Doubly Linked List */
-    static Node push(Node head, int new_data) {
-        // allocate node
-        Node new_node = new Node();
-
-        // put in the data
-        new_node.data = new_data;
-
-        /* since we are adding at the beginning,
-        prev is always NULL */
-        new_node.prev = null;
-
-        // link the old list off the new node
-        new_node.next = head;
-
-        // change prev of head node to new node
-        if (head != null) head.prev = new_node;
-
-        // move the head to point to the new node
-        head = new_node;
-
-        return head;
-    }
-
-    /* Function to print nodes in a given doubly
-        linked list */
-    static void printList(Node temp) {
-        if (temp == null) System.out.print(-1);
-
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-    }
-
-    // Driver code
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            Node head = null;
-            int n = sc.nextInt();
-            int inp[] = new int[n];
-            for (int i = 0; i < n; i++) inp[i] = sc.nextInt();
-            for (int i = n - 1; i >= 0; i--) {
-                head = push(head, inp[i]);
-            }
-            int x = sc.nextInt();
-            Solution obj = new Solution();
-            head = obj.deleteAllOccurOfX(head, x);
-            printList(head);
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 /* Structure of Doubly Linked List
 class Node
 {
-	int data;
-	Node next;
-	Node prev;
-	Node(int data)
-	{
-	    this.data = data;
-	    next = prev = null;
-	}
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
 }*/
 class Solution {
     static Node deleteAllOccurOfX(Node head, int x) {
-        if (head == null) return null;
+        // Write your code here
+        Node cur = head;
         
-        while (head != null && head.data == x) {
-            head = head.next;
+        while (cur != null && cur.data == x) {
+            cur = cur.next;
+            cur.prev = null;
+            head = cur;
         }
         
-        Node cur = head;
-        Node prev = null;
-        
         while (cur != null) {
-            if (cur.data == x) {
-                prev.next = cur.next;
-            } else prev = cur;
+            if (cur.data != x) {
+                cur = cur.next ;
+                continue;
+            }
+            
+            // tail
+            if (cur.next == null) {
+                cur.prev.next = null;
+                cur.prev = null;
+                cur = cur.next;
+                continue;
+            }
+            
+            cur.prev.next = cur.next;
+            cur.next.prev = cur.prev;
             
             cur = cur.next;
         }
