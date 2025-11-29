e8f8c39ee10455df922fe2de12d9a51eb7a46544
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-11-29 09:44:08 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.java
+++ Difficulty: Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.java
@@ -1,115 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-
-class Node
-{
-    int data;
-    Node next,prev;
-    
-    Node(int x)
-    {
-        data = x;
-        next = null;
-        prev = null;
-    }
-    
-    public static Node inputList(BufferedReader br) throws IOException
-    {
-        int n = Integer.parseInt(br.readLine().trim()); // Length of Linked List
-        
-        String[] s = br.readLine().trim().split(" ");
-        Node head = new Node(Integer.parseInt(s[0])), tail = head;
-        for(int i = 1; i < s.length; i++){
-            Node temp=new Node(Integer.parseInt(s[i]));
-            tail.next = temp;
-            tail.next.prev = tail;
-            tail = tail.next;
-        }
-        return head;
-    }
-    
-    public static void printList(Node node)
-    {
-        while (node != null)
-        { 
-    		System.out.print(node.data + " ");
-    		node = node.next; 
-    	}  
-    	System.out.println();
-    }
-}
-
-
-class IntMatrix
-{
-    public static int[][] input(BufferedReader br, int n, int m) throws IOException
-    {
-        int[][] mat = new int[n][];
-        
-        for(int i = 0; i < n; i++)
-        {
-            String[] s = br.readLine().trim().split(" ");
-            mat[i] = new int[s.length];
-            for(int j = 0; j < s.length; j++)
-                mat[i][j] = Integer.parseInt(s[j]);
-        }
-        
-        return mat;
-    }
-    
-    public static void print(int[][] m)
-    {
-        for(var a : m)
-        {
-            for(int e : a)
-                System.out.print(e + " ");
-            System.out.println();
-        }
-    }
-    
-    public static void print(ArrayList<ArrayList<Integer>> m)
-    {
-        if(m.size()==0){
-            System.out.println("-1");
-        }else{
-            for(var a : m)
-            {
-                System.out.print("(" + a.get(0) + "," + a.get(1) + ")" + " ");
-            }
-            System.out.println();
-        }
-    }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
-            
-            int target; 
-            target = Integer.parseInt(br.readLine());
-            
-            
-            Node head = Node.inputList(br);
-            
-            Solution obj = new Solution();
-            ArrayList<ArrayList<Integer>> res = obj.findPairsWithGivenSum(target, head);
-            
-            IntMatrix.print(res);
-            
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
-
-
 /*
 
 Definition for singly Link List Node
@@ -117,7 +5,7 @@ class Node
 {
     int data;
     Node next,prev;
-    
+
     Node(int x){
         data = x;
         next = null;
@@ -130,29 +18,32 @@ Node.printList(Node node);
 */
 
 class Solution {
-    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
+    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target,
+                                                                      Node head) {
         if (head == null) return null;
+        Node left = head;
+        Node right = head;
         
-        Node l = head;
-        Node r = head;
+        while (right.next != null) right = right.next;
         
-        while (r.next != null) r = r.next;
-        
+        // ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
         ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
         
-        while (l.data < r.data) {
-            int s = l.data + r.data;
+        while (left.data < right.data) {
+            int sum = left.data + right.data;
             
-            if (s == target) {
-                ans.add(new ArrayList(Arrays.asList(l.data, r.data)));
-                l = l.next;
-                r = r.prev;
-            } else if (s < target) {
-                l = l.next;
-            } else r = r.prev;
+            if (sum == target) {
+                // res.add(new ArrayList(Arrays.asList(left.data, right.data)));
+                ans.add(new ArrayList<>(Arrays.asList(left.data, right.data)));
+                left = left.next;
+                right = right.prev;
+            } else if (sum < target) {
+                left = left.next;
+            } else {
+                right = right.prev;
+            }
         }
         
         return ans;
     }
 }
-        
