c34f6b13f5fbde33d614d70530d8907126ee77c0
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-12-28 16:28:40 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -1,94 +1,42 @@
-//{ Driver Code Starts
-
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-public class Main {
-    static BufferedReader br;
-    static PrintWriter ot;
-
-    public static void main(String args[]) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        ot = new PrintWriter(System.out);
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int V = Integer.parseInt(br.readLine().trim());
-            int E = Integer.parseInt(br.readLine().trim());
-            List<List<int[]>> list = new ArrayList<>();
-            for (int i = 0; i < V; i++) list.add(new ArrayList<>());
-            for (int i = 0; i < E; i++) {
-                String[] s = br.readLine().trim().split(" ");
-                int a = Integer.parseInt(s[0]);
-                int b = Integer.parseInt(s[1]);
-                int c = Integer.parseInt(s[2]);
-                list.get(a).add(new int[] {b, c});
-                list.get(b).add(new int[] {a, c});
-            }
-            ot.println(new Solution().spanningTree(V, E, list));
-
-            ot.println("~");
-        }
-        ot.close();
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    static int spanningTree(int V, int E, List<List<int[]>> adj) {
-        int sum = 0;
-        PriorityQueue<Vertex> pq = new PriorityQueue<>(Comparator.comparingInt(v -> v.distance));
-        pq.add(new Vertex(0, 0));
-        boolean[] vis = new boolean[adj.size()];
-
+    record State(int wt, int node, int parent) {}
+    
+    public int spanningTree(int V, int[][] edges) {
+        // code here
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
+            graph.get(edge[1]).add(new int[]{edge[0], edge[2]});
+        }
+        
+        PriorityQueue<State> pq = new PriorityQueue<>(
+            (s1, s2) -> Integer.compare(s1.wt, s2.wt)    
+        );
+        boolean[] vis = new boolean[V];
+        
+        pq.offer(new State(0, 0, -1));
+        int ans = 0;
+        
         while (!pq.isEmpty()) {
-            Vertex v = pq.poll();
-
-            if (vis[v.label]) continue;
-
-            vis[v.label] = true;
-            sum += v.distance;
-
-            for (int i = 0; i < adj.get(v.label).size(); i++) {
-                int label = adj.get(v.label).get(i)[0];
-                int wt = adj.get(v.label).get(i)[1];
-
-                if (!vis[label]) {
-                    pq.add(new Vertex(label, wt));
-                }
+            State s = pq.poll();
+            
+            if (vis[s.node]) continue;
+            
+            vis[s.node] = true;
+            ans += s.wt;
+            
+            for (int[] adjNode: graph.get(s.node)) {
+                int next = adjNode[0];
+                int wt = adjNode[1];
+                
+                pq.offer(new State(wt, next, s.node));
             }
         }
-
-        return sum;
+        
+        return ans;
     }
 }
-
-class Vertex {
-        int label;
-        int distance;
-
-        public Vertex(int label, int distance) {
-            this.label = label;
-            this.distance = distance;
-        }
-
-        public int getLabel() {
-            return label;
-        }
-
-        public void setLabel(int label) {
-            this.label = label;
-        }
-
-        public int getDistance() {
-            return distance;
-        }
-
-        public void setDistance(int distance) {
-            this.distance = distance;
-        }
-    }
