6053842394aa1c2242a812f4e50f7a113aad3b95
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2025-12-30 18:38:02 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count of distinct substrings/count-of-distinct-substrings.java
+++ Difficulty: Medium/Count of distinct substrings/count-of-distinct-substrings.java
@@ -1,99 +1,53 @@
-/*You are required to complete this method */
-class GfG {
-    public static int countDistinctSubstring(String st) {
-        Trie trie = new Trie();
-        for (int i = 0; i < st.length(); i++)
-            trie.insert(st.substring(i));
+class Solution {
+    public static int countSubs(String s) {
+        int len = s.length();
         
-        return countNodes(trie.getRoot());
-    }
-    
-    // once the trie is built with all the suffixes
-    // we can just count the number of nodes.
-    // because it represents the distinct substrings
-    private static int countNodes(Node root) {
-        if (root == null) return 0;
+        Node root = new Node();
+        int count = 0;
         
-        int count = 1;
-        for (int i = 0; i < 26; i++) {
-            if (root.links[i] != null) 
-                count += countNodes(root.links[i]);
+        for (int i = 0; i < len; i++) {
+            Node node = root;
+            for (int j = i; j < len; j++) {
+                char c = s.charAt(j);
+
+                if (!node.contains(c)) {
+                    node.put(c, new Node());
+                    count++;
+                }
+                
+                node = node.next(c);
+            }
         }
         
         return count;
     }
-    
-}
-
-class Trie {
-    private Node root;
-
-    public Trie() {
-        root = new Node();
-    }
-    
-    public Node getRoot() {
-        return this.root;
-    }
-    
-    public void insert(String word) {
-        Node node = root;
-
-        for (char c: word.toCharArray()) {
-            if (!node.containsKey(c)) node.put(c, new Node());
-
-            node = node.get(c);
-        }
-
-        node.setEndOfWord();
-    }
-    
-    public boolean search(String word) {
-        Node node = root;
-
-        for (char c: word.toCharArray()) {
-            if (!node.containsKey(c)) return false;
-
-            node = node.get(c);
-        }
-
-        return node.isEndOfWord();
-    }
-    
-    public boolean startsWith(String prefix) {
-        Node node = root;
-
-        for (char c: prefix.toCharArray()) {
-            if (!node.containsKey(c)) return false;
-
-            node = node.get(c);
-        }
-
-        return true;
-    }
 }
 
 class Node {
-    Node links[] = new Node[26];
-    boolean endOfWord;
+    List<Node> links = new ArrayList<>();
+    boolean isEnd;
 
-    public boolean containsKey(char c) {
-        return links[c - 'a'] != null;
+    public Node() {
+        for (int i = 0; i < 26; i++) 
+            links.add(null);
     }
 
-    public void put(char c, Node node) {
-        links[c - 'a'] = node;
+    boolean contains(char c) {
+        int index = c - 'a';
+        return links.get(index) != null;
     }
 
-    public Node get(char c) {
-        return links[c - 'a'];
-    }
-    
-    public void setEndOfWord() {
-        this.endOfWord = true;
+    void put(char c, Node ref) {
+        int index = c - 'a';
+        links.set(index, ref);
     }
 
-    public boolean isEndOfWord() {
-        return this.endOfWord;
+    Node next(char c) {
+        int index = c - 'a';
+        return links.get(index);
+    }
+
+    void updateEnd() {
+        this.isEnd = true;
     }
 }
