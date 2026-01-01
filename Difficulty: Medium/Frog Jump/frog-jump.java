976a86502c6a03fa3798d6411732ee9b7db072da
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2026-01-01 17:29:46 +0000

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,28 +1,24 @@
-// User function Template for Java
 class Solution {
     int minCost(int[] height) {
+        // code here
         int len = height.length;
-        int memo[] = new int[len + 1];
+        int[] memo = new int[len];
+        
         Arrays.fill(memo, -1);
         
         return f(len - 1, height, memo);
-        
     }
     
-    int f(int ind, int[] height, int[] memo) {
+    private int f(int ind, int[] height, int[] memo) {
         if (ind == 0) return 0;
+        
         if (memo[ind] != -1) return memo[ind];
         
-        int oneStepCost = f(ind - 1, height, memo) + Math.abs(height[ind] - height[ind - 1]);
+        int oneJumpCost = f(ind - 1, height, memo) + Math.abs(height[ind] - height[ind - 1]);
+        int twoJumpCost = Integer.MAX_VALUE;
         
-        int twoStepCost = Integer.MAX_VALUE;
-        if (ind > 1)
-            twoStepCost = f(ind - 2, height, memo) + Math.abs(height[ind] - height[ind - 2]);
-            
-        int ans = Math.min(oneStepCost, twoStepCost);
+        if (ind > 1) twoJumpCost = f(ind - 2, height, memo) + Math.abs(height[ind] - height[ind - 2]);
         
-        memo[ind] = ans;
-        
-        return ans;
+        return memo[ind] = Math.min(oneJumpCost, twoJumpCost);
     }
 }
